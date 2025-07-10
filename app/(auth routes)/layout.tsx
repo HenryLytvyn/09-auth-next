97f07ea7aa2fe3b7fd30ec726243f8766e31756c
Henry <hennadii.lytvyn@gmail.com>
2025-07-10 22:19:36 +0100

    tototo

---
+++ app/(auth routes)/layout.tsx
@@ -0,0 +1,3 @@
+export default function authorizationLayout() {
+  return <div>authorizationLayout</div>;
+}
--- app/api/api.ts
+++ app/api/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: 'https://notehub-public.goit.study/api',
-  baseURL: 'https://notehub-api.goit.study',
-  // baseURL: 'http://localhost:3000/api',
+  // baseURL: 'https://notehub-api.goit.study',
+  baseURL: 'http://localhost:3000/api',
   withCredentials: true,
 });
