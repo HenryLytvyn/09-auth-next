cb3aa76b478db80d42a922da0afdb13d377975ed
Henry <hennadii.lytvyn@gmail.com>
2025-07-19 17:08:53 +0100

    AuthProvider edit

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -14,27 +14,40 @@ export default function AuthProvider({ children }: Props) {
     state => state.clearIsAuthenticated
   );
 
+  // useEffect(() => {
+  //   async function fetchUser() {
+  //     // Перевіряємо сесію
+  //     const isAuthenticated = await checkSession();
+  //     // if (isAuthenticated.message === 'No active session found') {
+  //     //   console.log('Error 400: No active session found');
+  //     //   return;
+  //     // }
+  //     // if (isAuthenticated.message === 'Invalid or expired token') {
+  //     //   console.log('Error 401: Invalid or expired token');
+  //     //   return;
+  //     // }
+
+  //     // isAuthenticated.message === 'Session refreshed successfully';
+
+  //     if (isAuthenticated) {
+  //       // Якщо сесія валідна — отримуємо користувача
+  //       const user = await getMe();
+  //       if (user) setUser(user);
+  //     } else {
+  //       // Якщо сесія невалідна — чистимо стан
+  //       clearIsAuthenticated();
+  //     }
+  //   }
+  //   fetchUser();
+  // }, [setUser, clearIsAuthenticated]);
+
   useEffect(() => {
     async function fetchUser() {
-      // Перевіряємо сесію
-      const isAuthenticated = await checkSession();
-      // if (isAuthenticated.message === 'No active session found') {
-      //   console.log('Error 400: No active session found');
-      //   return;
-      // }
-      // if (isAuthenticated.message === 'Invalid or expired token') {
-      //   console.log('Error 401: Invalid or expired token');
-      //   return;
-      // }
-
-      // isAuthenticated.message === 'Session refreshed successfully';
-
-      if (isAuthenticated) {
-        // Якщо сесія валідна — отримуємо користувача
+      try {
+        await checkSession();
         const user = await getMe();
-        if (user) setUser(user);
-      } else {
-        // Якщо сесія невалідна — чистимо стан
+        setUser(user);
+      } catch {
         clearIsAuthenticated();
       }
     }
